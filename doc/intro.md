74d9d4d78ac95154cbb9fedf1a5ecbc22f977af2
Martin Jul <martin@mjul.com>
2022-02-09 20:07:44 +0100

    Set up template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to core-logic-wordle
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/core_logic_wordle/core_test.clj
@@ -0,0 +1,7 @@
+(ns core-logic-wordle.core-test
+  (:require [clojure.test :refer :all]
+            [core-logic-wordle.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
